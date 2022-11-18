1c6d3da349b3d18f5f55134d5e1ec25890921167
쫑과이 <jjong@jjong-MacBookAir.local>
2022-11-18 10:48:02 +0900

    Event script

---
+++ src/Event/TC80_1.java
@@ -0,0 +1,68 @@
+package Event;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TC80_1 {
+
+    private static WebDriver driver;
+
+    @Test
+    public void TC40() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/mk-am14-030/Documents/selenium/chromedriver");
+
+        Map<String, String> mobileEmulation = new HashMap<>();
+
+        mobileEmulation.put("deviceName", "Samsung Galaxy S20 Ultra");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+
+        // stg 접속
+        driver.get("https://www.stg.kurly.com/member/login?return_url=/mypage");
+        Thread.sleep(1500);
+
+        // 현재창 핸들
+        String winHandleBefore = driver.getWindowHandle();
+
+        // 아이디 입력
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[3]/form/div[1]/div[1]/div/input")).sendKeys("webauto16");
+        Thread.sleep(500);
+
+        // 패스워드 입력
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[3]/form/div[1]/div[2]/div/input")).sendKeys("qawsedrf12");
+        Thread.sleep(500);
+
+        // 로그인 버튼 클릭
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[3]/form/div[3]/button[1]")).click();
+        Thread.sleep(1200);
+
+        // 장바구니 아이콘 클릭
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/div[1]/div/div/div[2]/button[2]")).click();
+        Thread.sleep(1500);
+
+        // 100원딜 구매권 미보유 계정
+        driver.findElement(By.xpath("//*[@id=\"__next\"]/footer/button")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals("이벤트 쿠폰이 유효하지 않습니다. 장바구니에서 아래 상품을 제외하고 다시 주문해주세요. (레고상품5)", driver.findElement(By.xpath("//*[@id=\"swal2-content\"]/div[1]")).getText());
+        System.out.println("구매권 미보유 얼럿 확인");
+        Thread.sleep(1000);
+
+        driver.quit();
+
+    }
+
+}
